d16c9437e4a8cbfff7370aa49ffaf603b24237cf
saeed <01shadowalker01@gmail.com>
2019-11-15 00:00:59 +0330

    settings in the CoreModule ...

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,11 +2,35 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { AlertModule } from "./modules/alert/alert.module";
 import { MAT_SNACK_BAR_DEFAULT_OPTIONS } from "@angular/material";
+import { HTTP_INTERCEPTORS, HttpClient } from "@angular/common/http";
+import { HttpErrorInterceptor } from "./services/http-interceptors/http-error.interceptor";
+import { AlertService } from "./modules/alert/shared/services/alert.service";
+import { TranslateModule, TranslateLoader } from "@ngx-translate/core";
+import { TranslateHttpLoader } from "@ngx-translate/http-loader";
+import { environment } from "src/environments/environment";
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, AlertModule],
+  imports: [
+    CommonModule,
+    AlertModule,
+
+    // 3rd party
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
+  ],
   providers: [
+    AlertService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpErrorInterceptor,
+      multi: true
+    },
     {
       provide: MAT_SNACK_BAR_DEFAULT_OPTIONS,
       useValue: {
@@ -17,3 +41,10 @@ import { MAT_SNACK_BAR_DEFAULT_OPTIONS } from "@angular/material";
   ]
 })
 export class CoreModule {}
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader();
+  // http,
+  // `${environment.i18nPrefix}/assets/i18n/`,
+  // '.json'
+}
